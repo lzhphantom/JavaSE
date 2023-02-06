49622864632505ddf94a4b4ac9246b5ed10a0b79
lzhphantom <lzhphantom2014@gmail.com>
2023-02-06 14:23:56 +0800

    Create Father.java
    同步代码块

---
+++ src/main/java/com/lzhphantom/se/Father.java
@@ -0,0 +1,11 @@
+package com.lzhphantom.se;
+
+/**
+ * @author luozhihui
+ * @date 2/6/2023
+ */
+public class Father {
+    public synchronized void say(){
+        System.out.println("我是你老汉儿");
+    }
+}
