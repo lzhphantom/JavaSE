8849cd3136237d8c1da2ba082284d2aaebcedc9f
lzhphantom <lzhphantom2014@gmail.com>
2023-02-15 16:50:41 +0800

    Create GetClass.java

---
+++ src/main/java/com/lzhphantom/instance/GetClass.java
@@ -0,0 +1,28 @@
+package com.lzhphantom.instance;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author lzhphantom
+ * @create 2/15/2023
+ */
+public class GetClass {
+    public void test1(){
+        GetClass getClass = new GetClass();
+        Class<? extends GetClass> clazz = getClass.getClass();
+        //获取全部方法
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        //全部构造方法
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        //全部属性
+        Field[] declaredFields = clazz.getDeclaredFields();
+    }
+    public void test2(){
+        Class<GetClass> getClassClass = GetClass.class;
+    }
+    public void test3() throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("sg.com.ncs.luozhihui.instance.GetClass");
+    }
+}
