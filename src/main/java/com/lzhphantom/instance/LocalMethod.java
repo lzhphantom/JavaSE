dc0db8457aaa2ad13510440b256938a48e6f9671
lzhphantom <lzhphantom2014@gmail.com>
2023-02-16 14:31:45 +0800

    Add files via upload

---
+++ src/main/java/com/lzhphantom/instance/LocalMethod.java
@@ -0,0 +1,29 @@
+package sg.com.ncs.luozhihui.instance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * 调用本地系统执行文件
+ *
+ * @author lzhphantom
+ * @create 2/16/2023
+ */
+public class LocalMethod {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String s;
+        StringBuilder sb = new StringBuilder();
+        Process process = Runtime.getRuntime().exec("ping localhost");
+        InputStream fis = process.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+        while ((s = bufferedReader.readLine()) != null){
+            sb.append(s).append("\n");
+        }
+        System.out.println(sb);
+        process.waitFor();
+        System.out.println(process.exitValue());
+        fis.close();
+    }
+}
---
+++ src/main/java/com/lzhphantom/instance/QRCodeTest.java
@@ -0,0 +1,24 @@
+package sg.com.ncs.luozhihui.instance;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import cn.hutool.extra.qrcode.QrConfig;
+
+import java.awt.*;
+
+/**
+ * @author lzhphantom
+ * @create 2/16/2023
+ */
+public class QRCodeTest {
+    public static void main(String[] args) {
+        //QrCodeUtil.generate("https://www.baidu.com/",300,300, FileUtil.file(
+        //        "d:/qrcode.jpg"));
+        QrConfig config = new QrConfig(300, 300);
+        config.setMargin(3);
+        config.setForeColor(Color.YELLOW.getRGB());
+        config.setBackColor(Color.GREEN.getRGB());
+        QrCodeUtil.generate("https://www.baidu.com/", config, FileUtil.file(
+                "d:/qrcode.jpg"));
+    }
+}
