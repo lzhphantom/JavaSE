f055dc44af5f53eda8ae075baf0ba3a172fe69c6
lzhphantom <lzhphantom2014@gmail.com>
2023-02-22 16:44:08 +0800

    Create WebSite.java

---
+++ src/main/java/com/lzhphantom/design/flyweight/WebSite.java
@@ -0,0 +1,10 @@
+package sg.com.ncs.luozhihui.design.flyweight;
+
+/**
+ * @author luozhihui
+ * @create 2/22/2023
+ */
+public abstract class WebSite {
+    public abstract void use();
+    public abstract void use(User user);
+}
