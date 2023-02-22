601de8b815bfb725f5a300ca10015ef39a13b2f8
lzhphantom <lzhphantom2014@gmail.com>
2023-02-22 17:18:45 +0800

    Create IMovie.java

---
+++ src/main/java/com/lzhphantom/design/proxy/IMovie.java
@@ -0,0 +1,10 @@
+package sg.com.ncs.luozhihui.design.proxy;
+
+/**
+ * @author luozhihui
+ * @create 2/22/2023
+ */
+public interface IMovie {
+    void play(String movieName);
+    void advertising(Boolean isBoforMovie,String txt);
+}
