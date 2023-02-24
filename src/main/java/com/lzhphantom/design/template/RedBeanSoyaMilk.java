05161f5017728a7baa7e07f6f7e24ec015d4737e
lzhphantom <lzhphantom2014@gmail.com>
2023-02-24 16:55:19 +0800

    Add files via upload

---
+++ src/main/java/com/lzhphantom/design/template/RedBeanSoyaMilk.java
@@ -0,0 +1,23 @@
+package sg.com.ncs.luozhihui.design.template;
+
+/**
+ * @author luozhihui
+ * @create 2/24/2023
+ */
+public class RedBeanSoyaMilk extends SoyaMilk{
+    @Override
+    void addCondiments() {
+        System.out.println(" 加入上好的红豆 ");
+    }
+
+    public static void main(String[] args) {
+        //制作红豆豆浆
+        System.out.println("----制作红豆豆浆----");
+        SoyaMilk redBeanSoyaMilk = new RedBeanSoyaMilk();
+        redBeanSoyaMilk.make();
+
+        System.out.println("----制作花生豆浆----");
+        SoyaMilk peanutSoyaMilk = new PeanutSoyaMilk();
+        peanutSoyaMilk.make();
+    }
+}
