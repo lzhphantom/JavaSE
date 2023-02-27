5e9abb5de327b7445ab6bd182dbe88734a716eac
lzhphantom <lzhphantom2014@gmail.com>
2023-02-27 17:52:23 +0800

    Create Client.java

---
+++ src/main/java/com/lzhphantom/design/mediator/Client.java
@@ -0,0 +1,11 @@
+package sg.com.ncs.luozhihui.design.mediator;
+
+/**
+ * @author luozhihui
+ * @create 2/27/2023
+ */
+public class Client {
+    public static void main(String[] args) {
+        LoginFrame login = new LoginFrame("mediator mode");
+    }
+}
